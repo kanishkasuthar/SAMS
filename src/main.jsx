c24ba2a6ffe825f81453db4253daac8eecc99bb4
kanishka suthar <kanishkasuthar@kanishkas-MacBook-Air.local>
2026-08-08 00:14:11 +0530

    Configure HashRouter and relative base path for GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
@@ -13,8 +13,8 @@ window.addEventListener('unhandledrejection', (event) => {
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
